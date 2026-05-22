19b3e1b98e11a1b9ce22274a998202df4694488c
Satnam Singh <ssingh1488@myseneca.ca>
2026-05-22 09:28:18 -0400

    Completed Lab 2

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Author: Vidhi Patel
+# Author ID: vmpatel41
+# Date Created: 2026/05/22
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
+
---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author: Satnam Singh
+# Author ID: ssingh1488
+# Date Created: 2026/05/22
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
+
